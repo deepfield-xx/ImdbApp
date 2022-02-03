275b909b1f98122739c0fca1c1e3245acfbf0465
tomwlod <df@deepfield.co>
2022-02-03 22:08:19 +0100

    Add Font files to the project

---
+++ ImdbApp/Extension/UIFont/UIFont+Extension.swift
@@ -0,0 +1,18 @@
+//
+//  UIFont+Extension.swift
+//  ImdbApp
+//
+//  Created by Tom on 03/02/2022.
+//
+
+import UIKit
+
+extension UIFont {
+    class func postGroteskBold(_ size: CGFloat) -> UIFont? {
+        return UIFont(name: "PostGrotesk-Bold", size: size)
+    }
+    
+    class func postGroteskBook(_ size: CGFloat) -> UIFont? {
+        return UIFont(name: "PostGrotesk-Book", size: size)
+    }
+}
