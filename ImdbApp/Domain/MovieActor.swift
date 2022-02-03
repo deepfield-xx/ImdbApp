ef861ef9a5793e1ebfc9f67fad0d7ae1f1b644ce
tomwlod <df@deepfield.co>
2022-02-03 22:21:52 +0100

    Add domain objects

---
+++ ImdbApp/Domain/MovieActor.swift
@@ -0,0 +1,14 @@
+//
+//  MovieActor.swift
+//  ImdbApp
+//
+//  Created by Tom on 03/02/2022.
+//
+
+import Foundation
+
+struct MovieActor: Codable {
+    let image: String
+    let name: String
+    let asCharacter: String
+}
---
+++ ImdbApp/Domain/MovieCast.swift
@@ -0,0 +1,12 @@
+//
+//  MovieCast.swift
+//  ImdbApp
+//
+//  Created by Tom on 03/02/2022.
+//
+
+import Foundation
+
+struct MovieCast: Codable {
+    let actors: [MovieActor]
+}
