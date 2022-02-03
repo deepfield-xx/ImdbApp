84c681e7dc726866dada2b7ed1d02ff1016fad51
tomwlod <df@deepfield.co>
2022-02-03 21:58:37 +0100

    Init commit

---
+++ ImdbApp/ViewControllers/MainViewController/MainViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ImdbApp
+//
+//  Created by Tom on 03/02/2022.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        
+    }
+
+
+}
+
